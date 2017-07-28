eaba455b16f3a29ec4617ab6f0e37ed1f52aab7a
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-07-28 09:48:32 -0400

    Edits for rerun of TOME_3004 s1 A

--- code/SCRIPT_pupilPipeline.m
+++ code/SCRIPT_pupilPipeline.m
@@ -72,7 +72,7 @@ pupilPipelineWrapper(pathParams, ...
 pathParams.subjectID = 'TOME_3004';
 pathParams.sessionDate = '091916';
 customKeyValue1 = {'LowResScaleCal*', 'pupilCircleThresh', 0.02, 'pupilRange', [10 60], 'pupilGammaCorrection', .75,'ellipseTransparentLB',[0, 0, 500, 0, -0.5*pi], 'frameMask', [60 40],'perimeterColor','r'};
-customKeyValue2 = {'rfMRI_REST_*', 'pupilRange', [30 100], 'pupilCircleThresh', 0.05, 'pupilGammaCorrection', .75,'skipRun', true};
+customKeyValue2 = {'rfMRI_REST_*', 'pupilRange', [30 100], 'pupilCircleThresh', 0.05, 'pupilGammaCorrection', .75};
 customKeyValue3 = {'dMRI_*', 'pupilRange', [30 100], 'pupilCircleThresh', 0.05, 'pupilGammaCorrection', .75 ,'ellipseTransparentLB',[0, 0, 500, 0, -0.5*pi]};
 customKeyValue4 = {'T*_*', 'pupilRange', [30 100], 'pupilCircleThresh', 0.05, 'pupilGammaCorrection', .75 ,'ellipseTransparentLB',[0, 0, 500, 0, -0.5*pi]};
 customKeyValues = {customKeyValue1; customKeyValue2; customKeyValue3;customKeyValue4};
