e57c593b52d0ddeb2722134985a04a5e9be51073
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2017-09-21 16:45:06 -0400

    Added DEMO_eyeResponse
    The actual eyeResponse step is not ready yet, but all the previous ones
    are working.

--- code/DEMO_eyeResponse.m
+++ code/DEMO_eyeResponse.m
@@ -1,17 +1,14 @@
 
-% DEMO_eyeTracking
+% DEMO_eyeResponse
 %
-% Demonstrate the gazeCalibration function.
-%
-% This function will work in the same sandbox folder as the one created by
-% DEMO_eyetracking.
+% Demonstrates the eyeResponse struct generation
 %
 % Make sure your machine is configured to work with ToolboxToolbox.
 %
 
 %% set paths and make directories
 % create test sandbox on desktop
-sandboxDir = '~/Desktop/gazeCalibrationDEMO';
+sandboxDir = '~/Desktop/eyeResponseDemo';
 if ~exist(sandboxDir,'dir')
     mkdir(sandboxDir)
 end
@@ -52,40 +49,70 @@ pathParams.dataOutputDirFull = fullfile(pathParams.dataOutputDirRoot, pathParams
     pathParams.subjectID, pathParams.sessionDate, pathParams.eyeTrackingDir);
 pathParams.controlFileDirFull = fullfile(pathParams.controlFileDirRoot, pathParams.projectSubfolder, ...
     pathParams.subjectID, pathParams.sessionDate, pathParams.eyeTrackingDir);
+pathParams.dataSourceDirFull = fullfile(pathParams.dataSourceDirRoot, pathParams.projectSubfolder, ...
+    pathParams.subjectID, pathParams.sessionDate, pathParams.eyeTrackingDir);
 
-% Since we are operating in a sandbox, create the data directory
-if ~exist(pathParams.dataOutputDirFull,'dir')
-    mkdir(pathParams.dataOutputDirFull)
-end
-% Download and unzip the calibration data if it is not already there
-calData = fullfile(pathParams.dataOutputDirFull,'gazeCalData.zip');
-if ~exist (calData,'file')
-    url = 'https://ndownloader.figshare.com/files/9329932?private_link=0f7b4fcc973c34f88a24';
-    system (['curl -L ' sprintf(url) ' > ' sprintf(calData)])
+% Download and unzip the demo data if it is not already there
+demoPackage = fullfile(sandboxDir,'eyeResponseDEMO.zip');
+if ~exist (demoPackage,'file')
+    url = 'https://ndownloader.figshare.com/files/9375664?private_link=c72125f96a0998ff9ff3';
+    system (['curl -L ' sprintf(url) ' > ' sprintf(demoPackage)])
     currentDir = pwd;
-    cd (pathParams.dataOutputDirFull)
-    unzip(calData)
+    cd (sandboxDir)
+    unzip(demoPackage)
     cd (currentDir)
 end
+
 
 %% Define some file names
 % run
 glintFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_glint.mat']);
 pupilFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_pupil.mat']);
 calibratedGazeFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_calibratedGaze.mat']);
-ltReportFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_report.mat']);
+ltReportFileName = fullfile(pathParams.dataSourceDirFull, [pathParams.runName '_report.mat']);
 timebaseFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_timebase.mat']);
 
-% calibration
+% size calibration
+sizeCalFactorsFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.runName '_sizeCalFactors.mat']);
+
+% gaze calibration
 targetsFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.gazeCalName '_targets.mat']);
 glintFileNameCAL = fullfile(pathParams.dataOutputDirFull, [pathParams.gazeCalName '_glint.mat']);
 pupilFileNameCAL = fullfile(pathParams.dataOutputDirFull, [pathParams.gazeCalName '_pupil.mat']);
-LTdatFileName = fullfile(pathParams.dataOutputDirFull,[pathParams.gazeCalName '_LTdat.mat']);
+LTdatFileName = fullfile(pathParams.dataSourceDirFull,[pathParams.gazeCalName '_LTdat.mat']);
 gazeDataFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.gazeCalName '_gazeCalData.mat']);
 gazeCalFactorsFileName = fullfile(pathParams.dataOutputDirFull, [pathParams.gazeCalName '_gazeCalFactors.mat']);
 
 
-%% Pull calibration data
+%% derive timebase
+timebase = deriveTimebaseFromLTData(glintFileName,ltReportFileName,'timebaseFileName', timebaseFileName, 'plotAlignment',true);
+
+
+%% size calibration
+% find calibration runs
+calRunNames = dir (fullfile(pathParams.dataOutputDirFull, 'RawScaleCal*pupil.mat'));
+sizeCalDataFilesNames = { ...
+    fullfile(calRunNames(1).folder, calRunNames(1).name(1:end-10)) ...
+    fullfile(calRunNames(2).folder, calRunNames(2).name(1:end-10)) ...
+    fullfile(calRunNames(3).folder, calRunNames(3).name(1:end-10)) ...
+    };
+
+% get size calibration values
+calcSizeCalFactors(sizeCalDataFilesNames, sizeCalFactorsFileName)
+
+% apply size calibration
+calibratedPupil = applySizeCalibration(pupilFileName,sizeCalFactorsFileName);
+
+
+%% plot timeseries of pupil size
+figure
+plot(calibratedPupil.width)
+ylim([0 10])
+xlabel('Frames')
+ylabel('Pupil width [mm]')
+title('Pupil width in mm')
+
+%% Pull gaze calibration data
 
 % 1. just pull out LT calibration data (example for those runs that do not
 % have a raw video for gaze calibration)
@@ -96,9 +123,13 @@ copyLTdatToGazeData(LTdatFileName,LTgazeDataFileName)
 targetInfoFile = LTdatFileName;
 makeTargetsFile(targetInfoFile,targetsFileName,'targetsInfoFileType','LiveTrack','targetsLayout','3x3grid','viewingDistance', 1065)
 calcGazeCalData(pupilFileNameCAL,glintFileNameCAL,targetsFileName,gazeDataFileName)
+
+
 %% compare calibration data from LT and raw video
 % here we just plot the apparent gaze location in pixels to see how the
-% LiveTrack data compares to the bayesian tracked data.
+% LiveTrack data compares to the bayesian tracked data. This comparison in
+% useful to derive "scaling factors" for those cases in which the only
+% option is to use the livetrack calibration.
 
 tmpLT = load(LTgazeDataFileName);
 ltGaze.X = tmpLT.gazeCalData.pupil.X - tmpLT.gazeCalData.glint.X;
@@ -113,11 +144,9 @@ figure
 plot(ltGaze.X, ltGaze.Y,'*')
 hold on
 plot(rawGaze.X, rawGaze.Y,'*')
-legend('LiveTrack', 'Bayesian fit')
+legend('LiveTrack', 'custom fit')
 title('Apparent Gaze location on screen')
 
-% NOTE: investigate if the shift might be due to a different approach to
-% the "square pixels" assumption
 
 %% calc gaze calibration params using the raw data
 
@@ -125,13 +154,9 @@ calcGazeCalFactors(gazeDataFileName,gazeCalFactorsFileName,'verbosity','full','s
 
 %% apply the calibration to the raw data
 
-applyGazeCalibration(pupilFileName,glintFileName,gazeCalFactorsFileName,calibratedGazeFileName)
+calibratedGaze = applyGazeCalibration(pupilFileName,glintFileName,gazeCalFactorsFileName,'calibratedGazeFileName',calibratedGazeFileName);
 
 %% plot the calibrated data in screen and polar coordinates
-
-% load gaze data
-
-load(calibratedGazeFileName);
 
 % scatter plots
 plotCalibratedGaze(calibratedGaze,'whichCoordSystem','screen','plotType','scatter')
@@ -142,5 +167,5 @@ plotCalibratedGaze(calibratedGaze,'whichCoordSystem','screen','plotType','timese
 plotCalibratedGaze(calibratedGaze,'whichCoordSystem','polar','plotType','timeseries')
 
 
-%% derive timebase
-deriveTimebaseFromLTData(glintFileName,ltReportFileName,timebaseFileName, 'plotAlignment',true)
+%% make eyeResponse struct
+% coming soon
