5068356acdd378d26abd8567f9970e6252a6af67
gkaguirre <gkaguirre@me.com>
2019-09-14 15:43:43 -0400

    Rename config, add add-on check
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- config/eyeTrackTOMEAnalysisLocalHookTemplate.m
+++ config/eyeTrackTOMEAnalysisLocalHookTemplate.m
@@ -11,8 +11,7 @@ function eyeTrackTOMEAnalysisLocalHook
 % You will need to edit the project location and i/o directory locations
 % to match what is true on your computer.
 
-%% Say hello
-fprintf('* Running eyeTrackTOMEAnalysisLocalHook...');
+
 
 %% Set preferences
 
@@ -22,3 +21,23 @@ projectDir = fullfile(tbLocateProject('eyeTrackTOMEAnalysis'));
 addpath(genpath(projectDir));
 setpref('eyeTrackTOMEAnalysis', 'projectDir', projectDir);
 
+
+%% Check for required Matlab toolboxes
+requiredAddOns = {...
+    'Mapping Toolbox'...                  % optimization_toolbox
+    };
+% Given this hard-coded list of add-on toolboxes, we then check for the
+% presence of each and issue a warning if absent.
+V = ver;
+VName = {V.Name};
+warnState = warning();
+warning off backtrace
+for ii=1:length(requiredAddOns)
+    if ~any(strcmp(VName, requiredAddOns{ii}))
+        warnString = ['The Matlab ' requiredAddOns{ii} ' is missing. ' toolboxName ' may not function properly.'];
+        warning('localHook:requiredMatlabToolboxCheck',warnString);
+    end
+end
+warning(warnState);
+
+end
