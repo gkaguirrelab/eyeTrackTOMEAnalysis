34f1c8519c40b641b811505615873325ac3845b7
gkaguirre <gkaguirre@me.com>
2020-07-25 07:04:39 -0400

    Only passing camera torsion and depth
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/submitSceneEstimation.m
+++ code/submitSceneEstimation.m
@@ -2,7 +2,7 @@ function submitSceneEstimation(subjectIdx)
 
 
 %% Obtain the scene analysis parameters
-[videoStemName, frameSet, gazeTargets, eyeArgs, sceneArgs, sceneParamsX0, kvals] = defineSubjectSceneParams;
+[videoStemName, frameSet, gazeTargets, eyeArgs, sceneArgs, torsDepth, kvals] = defineSubjectSceneParams;
 
 
 %% Loop over the subjectIdx
@@ -47,25 +47,20 @@ for ss = subjectIdx
     end
     
     
-    %% Obtain camera depth and torsion for each scene.
-    cameraDepth = cellfun(@(x) x(end),sceneParamsX0{ss});
-    cameraTorsion = cellfun(@(x) x(3),sceneParamsX0{ss});
-
+    %% Obtain camera torsion and depth for each scene.
+    cameraTorsion = cellfun(@(x) x(1),torsDepth{ss});
+    cameraDepth = cellfun(@(x) x(2),torsDepth{ss});
     
     %% Assemble the eye x0 params using the passed kvals
     model=[];
     if isempty(kvals{ss})
         % The mean corneal curvature in the TOME subjects, with 0 corneal
         % torsion, and 2.5 degrees of tilt
-        model.eye.x0 = [43.399, 44.33653846, 0, 2.5, 0, 0.91, 0.94, 0];        
-        model.eye.bounds = [3, 3, 180, 5, 2.5, 0.25, 0.25, 30];
+        model.eye.x0 = [43.399, 44.33653846, 0, 2.5, 0, 0.91, 0.94, 0];
     else
         model.eye.x0 = [kvals{ss}, 2.5, 0, 0.91, 0.94, 0];
     end
 
-    if length(sceneParamsX0{ss})==1
-        model.scene.x0 = sceneParamsX0{ss}{1};
-    end
     
     %% Constrain the eyePose bounds in the errorArgs
     errorArgs = {'eyePoseUB',[25,25,0,4],'eyePoseLB',[-25,-25,0,0.5]}; 
