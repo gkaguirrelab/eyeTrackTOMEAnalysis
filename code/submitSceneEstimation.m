ba51f0df3c144c2683574210e734c934502e9109
gkaguirre <gkaguirre@me.com>
2020-07-27 09:59:28 -0400

    Adjusting bounds on eye params
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/submitSceneEstimation.m
+++ code/submitSceneEstimation.m
@@ -57,9 +57,10 @@ for ss = subjectIdx
         % The mean corneal curvature in the TOME subjects, with 0 corneal
         % torsion, and 2.5 degrees of tilt
         model.eye.x0 = [43.399, 44.33653846, 0, 2.5, 0, 0.91, 0.94, 0];
+        model.eye.bounds = [3, 3, 180, 5, 2.5, 0.25, 0.25, 30];
     else
         model.eye.x0 = [kvals{ss}, 2.5, 0, 0.91, 0.94, 0];
-        model.eye.bounds = [1, 1, 180, 5, 2.5, 0.25, 0.25, 30];
+        model.eye.bounds = [0.5, 0.5, 30, 5, 2.5, 0.25, 0.25, 30];
     end
 
     
