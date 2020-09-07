55c657465d115e1d8b10af75a4b0d92379dd2c75
gkaguirre <gkaguirre@me.com>
2020-09-07 13:32:45 -0400

    Handle some re-plotting behavior
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/Aguirre_2020_MovingEye/recoverSimulatedEyePose.m
+++ code/Aguirre_2020_MovingEye/recoverSimulatedEyePose.m
@@ -2,9 +2,10 @@
 % figure panels for Figure 2 of Aguirre 2020, moving eye
 
 clear
+close all
 
-% Create a default sceneGeometry
-sceneGeometry=createSceneGeometry();
+% Set the re-plot flag
+replotFlag = true;
 
 % Define the set of pose values
 aziSet = -20:0.01:20;
@@ -21,6 +22,9 @@ perimNoise = 0.25;
 
 % Set a rmseThresh
 rmseThresh = max([1, perimNoise * 4]);
+
+% Create a default sceneGeometry
+sceneGeometry=createSceneGeometry();
 
 % Explicitly define the search args
 searchArgs = {'eyePoseEllipseFitFunEvals',250,'glintTol',1,'eyePoseTol',1e-3};
@@ -52,41 +56,55 @@ for cc = 1:3
             cameraTransBounds = [20; 20; 0];
     end
     
-    % Loop though simulations
-    simSet = [];
-    for ss = 1:nSims
-        eyePose(ss,:) = [randsample(aziSet,1) randsample(eleSet,1) 0 randsample(stopSet,1)];
-        cameraTrans(:,ss) = [randsample(xSet,1); randsample(ySet,1); randsample(zSet,1)];
-        [ targetEllipse, glintCoordOrig ] = projectModelEye(eyePose(ss,:),sceneGeometry,'cameraTrans',cameraTrans(:,ss));
+    % This is where we will save the simulation state
+    stateSaveName = sprintf('/Users/aguirre/Desktop/simStateSave_%d.mat',cc);
+    
+    % Do we want to re-do the simulations, or just load them from disk?
+    if ~replotFlag
         
-        % Obtain the glintCoord and perimeter points of the ellipse, adding
-        % noise
-        glintCoord = glintCoordOrig + randn(size(glintCoordOrig)).*perimNoise;
-        [ Xp, Yp ] = ellipsePerimeterPoints( targetEllipse, 10, 0, perimNoise );
+        % Loop though simulations
+        simSet = [];
+        for ss = 1:nSims
+            eyePose(ss,:) = [randsample(aziSet,1) randsample(eleSet,1) 0 randsample(stopSet,1)];
+            cameraTrans(:,ss) = [randsample(xSet,1); randsample(ySet,1); randsample(zSet,1)];
+            [ targetEllipse, glintCoordOrig ] = projectModelEye(eyePose(ss,:),sceneGeometry,'cameraTrans',cameraTrans(:,ss));
             
-        % Get the eye pose, and repeat if the error is above threshold
-        [eyePoseRecovered(ss,:), cameraTransRecovered(:,ss), RMSE(ss)] = eyePoseEllipseFit(Xp, Yp, glintCoord, sceneGeometry,'cameraTransBounds',cameraTransBounds,searchArgs{:});
-        if RMSE(ss)>rmseThresh
-            [eyePoseRecovered(ss,:), cameraTransRecovered(:,ss), RMSE(ss)] = eyePoseEllipseFit(Xp, Yp, glintCoord, sceneGeometry,'cameraTransBounds',cameraTransBounds,'eyePoseX0',eyePoseRecovered(ss,:),searchArgs{:});
-            if RMSE(ss) > rmseThresh
-                fprintf('Damnit.\n')
-                eyePose(ss,:) = nan;
-                cameraTrans(:,ss) = nan;
-                eyePoseRecovered(ss,:) = nan;
-                cameraTransRecovered(:,ss) = nan;
+            % Obtain the glintCoord and perimeter points of the ellipse, adding
+            % noise
+            glintCoord = glintCoordOrig + randn(size(glintCoordOrig)).*perimNoise;
+            [ Xp, Yp ] = ellipsePerimeterPoints( targetEllipse, 10, 0, perimNoise );
+            
+            % Get the eye pose, and repeat if the error is above threshold
+            [eyePoseRecovered(ss,:), cameraTransRecovered(:,ss), RMSE(ss)] = eyePoseEllipseFit(Xp, Yp, glintCoord, sceneGeometry,'cameraTransBounds',cameraTransBounds,searchArgs{:});
+            if RMSE(ss)>rmseThresh
+                [eyePoseRecovered(ss,:), cameraTransRecovered(:,ss), RMSE(ss)] = eyePoseEllipseFit(Xp, Yp, glintCoord, sceneGeometry,'cameraTransBounds',cameraTransBounds,'eyePoseX0',eyePoseRecovered(ss,:),searchArgs{:});
+                if RMSE(ss) > rmseThresh
+                    fprintf('Damnit.\n')
+                    eyePose(ss,:) = nan;
+                    cameraTrans(:,ss) = nan;
+                    eyePoseRecovered(ss,:) = nan;
+                    cameraTransRecovered(:,ss) = nan;
+                end
             end
+            
         end
         
+        save(stateSaveName)
+        
+    else
+        tmpFigHandle = figHandle;
+        load(stateSaveName)
+        close(figHandle);
+        figHandle = tmpFigHandle;
     end
     
-    stateSaveName = sprintf('/Users/aguirre/Desktop/simStateSave_%d.mat',cc);
-    save(stateSaveName)
-
+    % Add to the figure
     makeFig(eyePose,eyePoseRecovered,cameraTrans,cameraTransRecovered,RMSE,rmseThresh,cc);
 end
 
+% Save the figure
 fileName = '/Users/aguirre/Desktop/simFig.pdf';
-print(figHandle,fileName)
+print(figHandle,fileName,'-dpdf')
 
 % Turn the warning back on
 warning('off','eyePoseEllipseFit:underconstrainedSearch')
@@ -95,7 +113,8 @@ warning('off','eyePoseEllipseFit:underconstrainedSearch')
 
 function makeFig(eyePose,eyePoseRecovered,cameraTrans,cameraTransRecovered,RMSE,rmseThresh,cc)
 
-alphaVal = min([20/size(eyePose,1) 1]);
+alphaVal = min([50/size(eyePose,1) 1]);
+markerSize = 10;
 
 lowRMSE = RMSE < rmseThresh;
 
@@ -107,7 +126,7 @@ pctSet = [5,95,50];
 lineSpec = {'--k','--k','-k'};
 
 
-subplot(3,4,(cc-1)*4+1)
+subplot(4,3,1+(cc-1))
 hold off
 error = vecnorm((eyePose(lowRMSE,1:2) - eyePoseRecovered(lowRMSE,1:2))')';
 medianError = nanmedian(error);
@@ -115,7 +134,7 @@ errorByDepth = corr(deltaDepth,error);
 errorByRot = corr(absRot,error);
 sim = [ eyePose(lowRMSE,1); eyePose(lowRMSE,2) ];
 rec = [ eyePoseRecovered(lowRMSE,1); eyePoseRecovered(lowRMSE,2) ];
-scatter(sim,sim-rec,50,'MarkerFaceColor',markerColor,'MarkerEdgeColor','none',...
+scatter(sim,sim-rec,markerSize,'MarkerFaceColor',markerColor,'MarkerEdgeColor','none',...
     'MarkerFaceAlpha',alphaVal);
 hold on
 D = [sim,sim-rec];
@@ -130,11 +149,12 @@ xlim([-20 20])
 ylim([-4 4])
 xlabel('simulated eye rotation [deg]');
 ylabel('error [deg]');
+axis square
 str = sprintf('error, Rd, Rr [%2.2f, %2.2f, %2.2f]',medianError,errorByDepth,errorByRot);
 title(str)
 
 
-subplot(3,4,(cc-1)*4+2)
+subplot(4,3,4+(cc-1))
 hold off
 sim = eyePose(lowRMSE,4);
 rec = eyePoseRecovered(lowRMSE,4);
@@ -142,7 +162,7 @@ error = abs(sim-rec);
 medianError = nanmedian(error);
 errorByDepth = corr(deltaDepth,error);
 errorByRot = corr(absRot,error);
-scatter(sim,sim-rec,50,'MarkerFaceColor',markerColor,'MarkerEdgeColor','none',...
+scatter(sim,sim-rec,markerSize,'MarkerFaceColor',markerColor,'MarkerEdgeColor','none',...
     'MarkerFaceAlpha',alphaVal);
 hold on
 D = [sim,sim-rec];
@@ -157,11 +177,12 @@ xlim([0 4]);
 ylim([-0.5 0.5]);
 xlabel('simulated stop radius [mm]');
 ylabel('error [mm]');
+axis square
 str = sprintf('error, Rd, Rr [%2.2f, %2.2f, %2.2f]',medianError,errorByDepth,errorByRot);
 title(str)
 
 
-subplot(3,4,(cc-1)*4+3)
+subplot(4,3,7+(cc-1))
 hold off
 error = vecnorm((cameraTrans(1:2,lowRMSE)-cameraTransRecovered(1:2,lowRMSE)))';
 medianError = nanmedian(error);
@@ -169,7 +190,7 @@ errorByDepth = corr(deltaDepth,error);
 errorByRot = corr(absRot,error);
 sim = [ cameraTrans(1,lowRMSE)'; cameraTrans(2,lowRMSE)' ];
 rec = [ cameraTransRecovered(1,lowRMSE)'; cameraTransRecovered(2,lowRMSE)' ];
-scatter(sim,sim-rec,50,'MarkerFaceColor',markerColor,'MarkerEdgeColor','none',...
+scatter(sim,sim-rec,markerSize,'MarkerFaceColor',markerColor,'MarkerEdgeColor','none',...
     'MarkerFaceAlpha',alphaVal);
 hold on
 D = [sim,sim-rec];
@@ -184,11 +205,12 @@ xlim([-10 10])
 ylim([-2 2])
 xlabel('simulated in-plane trans [mm]');
 ylabel('error [mm]');
+axis square
 str = sprintf('error, Rd, Rr [%2.2f, %2.2f, %2.2f]',medianError,errorByDepth,errorByRot);
 title(str)
 
 
-subplot(3,4,(cc-1)*4+4)
+subplot(4,3,10+(cc-1))
 hold off
 error = abs(cameraTrans(3,lowRMSE)-cameraTransRecovered(3,lowRMSE))';
 medianError = nanmedian(error);
@@ -196,7 +218,7 @@ errorByDepth = corr(deltaDepth,error);
 errorByRot = corr(absRot,error);
 sim = cameraTrans(3,lowRMSE)';
 rec = cameraTransRecovered(3,lowRMSE)';
-scatter(sim,sim-rec,50,'MarkerFaceColor',markerColor,'MarkerEdgeColor','none',...
+scatter(sim,sim-rec,markerSize,'MarkerFaceColor',markerColor,'MarkerEdgeColor','none',...
     'MarkerFaceAlpha',alphaVal);
 hold on
 D = [sim,sim-rec];
@@ -212,6 +234,7 @@ xlim([-10 10])
 ylim([-10 10])
 xlabel('simulated depth trans [mm]');
 ylabel('error [mm]');
+axis square
 str = sprintf('error, Rd, Rr [%2.2f, %2.2f, %2.2f]',medianError,errorByDepth,errorByRot);
 title(str)
 
