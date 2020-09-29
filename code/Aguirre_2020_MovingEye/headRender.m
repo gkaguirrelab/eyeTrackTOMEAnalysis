452045bd02e4a1ec76456b7c7dca44fbe13c8b7a
gkaguirre <gkaguirre@me.com>
2020-09-29 17:41:20 -0400

    Working on this figure
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/Aguirre_2020_MovingEye/headRender.m
+++ code/Aguirre_2020_MovingEye/headRender.m
@@ -7,7 +7,7 @@ fv = stlread(stlFileHead);
 
 
 
-headObj = patch(fv,'FaceColor',       [0.8 0.8 1.0], ...
+headObj = patch(fv,'FaceColor',       [210 161 140]./255, ...
          'EdgeColor',       'none',        ...
          'FaceLighting',    'gouraud',     ...
          'AmbientStrength', 0.15);
@@ -15,22 +15,35 @@ headObj.FaceAlpha = 0.1;
 direction = [0 0 1];
 rotate(headObj,direction,90)
 
+
+m = eye(4,4);
+m(1,4) = -90;
+m(2,4) = 32;
+m(3,4) = -8;
+
+t = hgtransform('Parent',gca);
+set(headObj,'Parent',t)
+set(t,'Matrix',m)
+
+
+
+
 stlFileBrain = '/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/_Papers/Aguirre_2020_movingEye/Figures/STL_files/rh.pial.stl';
 fv = stlread(stlFileBrain);
 
-brainObj = patch(fv,'FaceColor',       [1.0 0.8 0.8], ...
+brainObj = patch(fv,'FaceColor',       [1.0 0.5 0.5], ...
          'EdgeColor',       'none',        ...
          'FaceLighting',    'gouraud',     ...
-         'AmbientStrength', 0.15);
-brainObj.FaceAlpha = 0.25;
+         'AmbientStrength', 0.25);
+brainObj.FaceAlpha = 0.1;
 direction = [0 0 -1];
 rotate(brainObj,direction,90)
 
 
 m = eye(4,4);
-m(1,4) = -10;
-m(2,4) = 20;
-m(3,4) = 0;
+m(1,4) = -25;
+m(2,4) = 80;
+m(3,4) = -8;
 
 t = hgtransform('Parent',gca);
 set(brainObj,'Parent',t)
@@ -43,13 +56,27 @@ hold on
 sceneGeometry = createSceneGeometry();
 [~,p]=plotOpticalSystem('surfaceSet',sceneGeometry.refraction.retinaToCamera,'newFigure',false,'surfaceAlpha',0.75);
 
+
+cameraHandles = addCameraIcon(sceneGeometry);
+
+
+gazeTargets = [
+    7     0     0    -7     7    -7    -7     7     0
+    0     0     7    -7    -7     7     0     7    -7];
+screenHandles = addScreenIcon(sceneGeometry,[0 -90],gazeTargets);
 m = eye(4,4);
-m(1,4) = 90;
-m(2,4) = -32;
-m(3,4) = 8;
+m(1,4) = -120;
+m(2,4) = 32;
+m(3,4) = -8;
 
 t = hgtransform('Parent',gca);
-set(p,'Parent',t)
+set(screenHandles,'Parent',t)
 set(t,'Matrix',m)
 
-ylim([-100 0]);
+
+% Add the scanner bore
+S = quadric.scale(quadric.unitSphere,[300 300 50000]);
+S = quadric.translate(S,[-100 32 0]);
+boundingBox = [-400 400 -400 400 -200 1000];
+boreHandle = quadric.plotSurface(S, boundingBox, [0.5 0.5 1], 0.5);
+
