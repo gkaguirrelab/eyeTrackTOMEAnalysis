4ed75dcc73f961935c40eb2bf96b456681d0d9b1
gkaguirre <gkaguirre@me.com>
2020-12-15 08:59:46 -0500

    Starting on this figure
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ code/Aguirre_2020_MovingEye/figure7_headToEyeMotion.m
@@ -0,0 +1,7 @@
+
+% Plot the brain motion and eye motion parametes for TOME_3029, movie run 2
+
+headMotionFile = '/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/TOME_analysis/deriveCameraPositionFromHeadMotion/session2_spatialStimuliTOME_3029_tfMRI_MOVIE_AP_run2_Movement_Regressors.txt';
+headMotion = readmatrix(headMotionFile);
+
+
