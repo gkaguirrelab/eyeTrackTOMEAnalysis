7e1d7c7507960161c1c8441807ad952e5cb2dc14
gkaguirre <gkaguirre@me.com>
2021-01-03 09:22:19 -0500

    Adding some bumped to the start and end of the gif
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/Aguirre_2020_MovingEye/sceneSearchGif.m
+++ code/Aguirre_2020_MovingEye/sceneSearchGif.m
@@ -1,33 +1,35 @@
 % Create a gif that illustrates the scene search process
 
-% clear
-% close all
-% 
-% % Use the sceneGeometry from an example subject (TOME_3021)
-% videoStemName = '/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/TOME_processing/session2_spatialStimuli/TOME_3021/060917/EyeTracking/GazeCal01';
-% load([videoStemName '_sceneGeometry.mat'],'sceneGeometry');
-% 
-% % Extract from the existing sceneGeometry the input parameters that we need
-% % to process just this single sceneGeometry file
-% frameSet = sceneGeometry.meta.estimateSceneParams.obj.frameSet;
-% gazeTargets = sceneGeometry.meta.estimateSceneParams.obj.gazeTargets;
-% eyeArgs = sceneGeometry.meta.estimateSceneParams.obj.meta.eyeArgs;
-% sceneArgs = sceneGeometry.meta.estimateSceneParams.obj.meta.sceneArgs{2};
-% errorArgs = sceneGeometry.meta.estimateSceneParams.obj.meta.errorArgs;
-% cameraDepth = sceneGeometry.meta.estimateSceneParams.obj.meta.cameraDepth(2);
-% cameraTorsion = sceneGeometry.meta.estimateSceneParams.obj.meta.cameraTorsion(2);
-% 
-% % Prevent a search across camera depth by setting the bounds in the last
-% % position to zero for eye and scene.
-% model.eye.bounds = [5, 5, 5, 180, 5, 2.5, 0.25, 0.25, 0];
-% model.scene.bounds = [10 10 10 20 20 0];
-% 
-% % Run the search
-% sceneObject = estimateSceneParams(videoStemName,frameSet,gazeTargets, ...
-%     'model',model,...
-%     'eyeArgs',eyeArgs,'sceneArgs',sceneArgs,'errorArgs',errorArgs,...
-%     'cameraDepth',cameraDepth,'cameraTorsion',cameraTorsion,...
-%     'searchStrategy','gazeCal','savePlots',false,'saveFiles',false);
+clear
+close all
+
+% Use the sceneGeometry from an example subject (TOME_3021)
+dropboxBaseDir = getpref('eyeTrackTOMEAnalysis','dropboxBaseDir'); 
+processingBaseDir = fullfile(dropboxBaseDir,'TOME_processing');
+videoStemName = fullfile(processingBaseDir,'/session2_spatialStimuli/TOME_3021/060917/EyeTracking/GazeCal01');
+load([videoStemName '_sceneGeometry.mat'],'sceneGeometry');
+
+% Extract from the existing sceneGeometry the input parameters that we need
+% to process just this single sceneGeometry file
+frameSet = sceneGeometry.meta.estimateSceneParams.obj.frameSet;
+gazeTargets = sceneGeometry.meta.estimateSceneParams.obj.gazeTargets;
+eyeArgs = sceneGeometry.meta.estimateSceneParams.obj.meta.eyeArgs;
+sceneArgs = sceneGeometry.meta.estimateSceneParams.obj.meta.sceneArgs{2};
+errorArgs = sceneGeometry.meta.estimateSceneParams.obj.meta.errorArgs;
+cameraDepth = sceneGeometry.meta.estimateSceneParams.obj.meta.cameraDepth(2);
+cameraTorsion = sceneGeometry.meta.estimateSceneParams.obj.meta.cameraTorsion(2);
+
+% Prevent a search across camera depth by setting the bounds in the last
+% position to zero for eye and scene.
+model.eye.bounds = [5, 5, 5, 180, 5, 2.5, 0.25, 0.25, 0];
+model.scene.bounds = [10 10 10 20 20 0];
+
+% Run the search
+sceneObject = estimateSceneParams(videoStemName,frameSet,gazeTargets, ...
+    'model',model,...
+    'eyeArgs',eyeArgs,'sceneArgs',sceneArgs,'errorArgs',errorArgs,...
+    'cameraDepth',cameraDepth,'cameraTorsion',cameraTorsion,...
+    'searchStrategy','gazeCal','savePlots',false,'saveFiles',false);
 
 % Get the x parameter history
 xHist = sceneObject{1}.xHist;
@@ -36,32 +38,53 @@ xHist = sceneObject{1}.xHist;
 gifSaveName = '~/Desktop/sceneSearch.gif';
 
 % Loop over the iterations, plot, and save to the gif
-for ii = 1:size(xHist,1)
+for ii = 0:size(xHist,1)
     
-    % Get this set of parameeters
-    x = squeeze(sceneObject{1}.xHist(ii,:));
-    
-    if ii==1
+    fileNameSuffix = sprintf('_iter%0.3d',ii);
+        
+    if ii==0
+        
+        % fill x with nans, and get the montage with just the eye image
+        x = nan(size(squeeze(sceneObject{1}.xHist(1,:))));
         xLast = x;
-    else
-        x = squeeze(sceneObject{1}.xHist(ii-1,:));
+        sceneObject{1}.saveEyeModelMontage(fileNameSuffix,true,false,false)        
+
+        % Get the values for this iteration
+        modelPoseGaze = nan(size(sceneObject{1}.modelPoseGaze));
+        rawErrors = nan(size(sceneObject{1}.rawErrors));
+
+else
+        
+        % Get this set of parameters
+        x = squeeze(sceneObject{1}.xHist(ii,:));
+        
+        if ii==1
+            xLast = x;
+        else
+            xLast = squeeze(sceneObject{1}.xHist(ii-1,:));
+        end
+        
+        % Update the scene object for this iteration
+        sceneObject{1}.x=x;
+        sceneObject{1}.updateScene;
+        sceneObject{1}.updateError(sceneObject{1}.errorArgs{:});
+        
+        % Create a montage of the model for this iteration, load it into
+        % memory, and delete the version saved to disk
+        sceneObject{1}.saveEyeModelMontage(fileNameSuffix,true,true,false)
+        
+        % Get the values for this iteration
+        modelPoseGaze = sceneObject{1}.modelPoseGaze;
+        rawErrors = sceneObject{1}.rawErrors;
+
     end
     
-    % Update the scene object for this iteration
-    sceneObject{1}.x=x;
-    sceneObject{1}.updateScene;
-    sceneObject{1}.updateError(sceneObject{1}.errorArgs{:});
-    
-    % Create a montage of the model for this iteration, load it into
-    % memory, and delete the version saved to disk
-    fileNameSuffix = sprintf('_iter%0.3d',ii);
-    sceneObject{1}.saveEyeModelMontage(fileNameSuffix,true,true)
     fileName = [videoStemName '_sceneGeometry_eyeModelMontage' fileNameSuffix '.png'];
     montageImage = imread(fileName);
     delete(fileName);
     
     % Setup a figure
-    if ii==1
+    if ii==0
         figHandle = figure;
         figHandle.Position = [91 146 1130 659];
         figHandle.Color = 'w';
@@ -79,11 +102,7 @@ for ii = 1:size(xHist,1)
     
     % Plot the ability of the rotation values assigned to each eyePose to map
     % to the list of gaze targets
-    
-    % Get the values for this iteration
-    modelPoseGaze = sceneObject{1}.modelPoseGaze;
-    rawErrors = sceneObject{1}.rawErrors;
-    
+        
     % Plot gaze error
     nexttile([1 1])
     plot(gazeTargets(1,:),gazeTargets(2,:),'ok','MarkerSize',10); hold on;
@@ -138,10 +157,13 @@ for ii = 1:size(xHist,1)
     set(gca, 'YTick', []);
     box off
     
-    % Update the gif
-    if ii==1
+    % Setup or update the gif
+    if ii==0
         gif(gifSaveName,'frame',figHandle);
         gif('frame',figHandle);
+        for jj=1:10; gif('frame',figHandle); end        
+    elseif ii==size(xHist,1)
+        for jj=1:10; gif('frame',figHandle); end        
     else
         gif('frame',figHandle);
         gif('frame',figHandle);
